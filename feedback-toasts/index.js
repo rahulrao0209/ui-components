803dfc855008c6d45d6cea8c3a7a0557138df59a
unknown <rahulrao0209@gmail.com>
2022-07-21 21:08:25 +0530

    updated the styling | added logic to display different toasts based on button clicks

--- feedback-toasts/index.js
+++ feedback-toasts/index.js
@@ -1,18 +1,38 @@
-/* Selecting all buttons */
+/* Selecting the button container */
 var buttonContainer = document.querySelector(".button-container");
-var successBtn = document.querySelector(".button--success");
-var warningBtn = document.querySelector(".button--warning");
-var errorBtn = document.querySelector(".button--error");
 /* Selecting all toasts */
 var successToast = document.querySelector(".toast--success");
 var warningToast = document.querySelector(".toast--warning");
 var errorToast = document.querySelector(".toast--error");
 var toggleToast = function (event) {
     var button = event.target.closest("button");
-    button && (successToast === null || successToast === void 0 ? void 0 : successToast.classList.toggle("hide"));
+    var success = button === null || button === void 0 ? void 0 : button.classList.contains("button--success");
+    var warning = button === null || button === void 0 ? void 0 : button.classList.contains("button--warning");
+    var error = button === null || button === void 0 ? void 0 : button.classList.contains("button--error");
+    if (button) {
+        if (success) {
+            warningToast === null || warningToast === void 0 ? void 0 : warningToast.classList.add("hide");
+            errorToast === null || errorToast === void 0 ? void 0 : errorToast.classList.add("hide");
+            successToast === null || successToast === void 0 ? void 0 : successToast.classList.toggle("hide");
+        }
+        if (warning) {
+            errorToast === null || errorToast === void 0 ? void 0 : errorToast.classList.add("hide");
+            successToast === null || successToast === void 0 ? void 0 : successToast.classList.add("hide");
+            warningToast === null || warningToast === void 0 ? void 0 : warningToast.classList.toggle("hide");
+        }
+        if (error) {
+            successToast === null || successToast === void 0 ? void 0 : successToast.classList.add("hide");
+            warningToast === null || warningToast === void 0 ? void 0 : warningToast.classList.add("hide");
+            errorToast === null || errorToast === void 0 ? void 0 : errorToast.classList.toggle("hide");
+        }
+    }
 };
 /* Add click event listeners to the buttons */
 var init = function () {
     buttonContainer === null || buttonContainer === void 0 ? void 0 : buttonContainer.addEventListener("click", toggleToast);
+    /* All toasts are initially hidden */
+    successToast === null || successToast === void 0 ? void 0 : successToast.classList.add("hide");
+    warningToast === null || warningToast === void 0 ? void 0 : warningToast.classList.add("hide");
+    errorToast === null || errorToast === void 0 ? void 0 : errorToast.classList.add("hide");
 };
 init();
--- feedback-toasts/index.ts
+++ feedback-toasts/index.ts
@@ -1,8 +1,5 @@
-/* Selecting all buttons */
+/* Selecting the button container */
 const buttonContainer = document.querySelector(".button-container");
-const successBtn = document.querySelector(".button--success");
-const warningBtn = document.querySelector(".button--warning");
-const errorBtn = document.querySelector(".button--error");
 
 /* Selecting all toasts */
 const successToast = document.querySelector(".toast--success");
@@ -11,12 +8,43 @@ const errorToast = document.querySelector(".toast--error");
 
 const toggleToast = (event: any) => {
   const button = event.target.closest("button");
-  button && successToast?.classList.toggle("hide");
+
+  const success = button?.classList.contains("button--success");
+  const warning = button?.classList.contains("button--warning");
+  const error = button?.classList.contains("button--error");
+
+  if (button) {
+    if (success) {
+      warningToast?.classList.add("hide");
+      errorToast?.classList.add("hide");
+
+      successToast?.classList.toggle("hide");
+    }
+
+    if (warning) {
+      errorToast?.classList.add("hide");
+      successToast?.classList.add("hide");
+
+      warningToast?.classList.toggle("hide");
+    }
+
+    if (error) {
+      successToast?.classList.add("hide");
+      warningToast?.classList.add("hide");
+
+      errorToast?.classList.toggle("hide");
+    }
+  }
 };
 
 /* Add click event listeners to the buttons */
 const init = () => {
   buttonContainer?.addEventListener("click", toggleToast);
+
+  /* All toasts are initially hidden */
+  successToast?.classList.add("hide");
+  warningToast?.classList.add("hide");
+  errorToast?.classList.add("hide");
 };
 
 init();

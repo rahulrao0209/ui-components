c0d75ab866ea0a8e7b07cdad0fb21ace494d0b71
unknown <rahulrao0209@gmail.com>
2022-07-29 15:36:19 +0530

    completed form animation

--- multistep-form/index.js
+++ multistep-form/index.js
@@ -7,18 +7,32 @@ var nextStep1 = document.querySelector("#next-step-1");
 var nextStep2 = document.querySelector("#next-step-2");
 var prevStep2 = document.querySelector("#prev-step-2");
 var prevStep3 = document.querySelector("#prev-step-3");
+var animationClasses = ["disappear", "disappear-rev", "appear", "appear-rev"];
+var removeAnimationClasses = function (step) {
+    step === null || step === void 0 ? void 0 : step.classList.forEach(function (stepClass) {
+        if (animationClasses.includes(stepClass))
+            step.classList.remove(stepClass);
+    });
+};
 var handleNext = function (event) {
     event.preventDefault();
     var stepClicked = parseInt(event.target.dataset.val);
     // On step 1
     if (stepClicked === 1) {
-        step1 === null || step1 === void 0 ? void 0 : step1.classList.remove("disappear-rev");
-        step2 === null || step2 === void 0 ? void 0 : step2.classList.remove("appear-rev");
+        removeAnimationClasses(step1);
+        removeAnimationClasses(step2);
         step1 === null || step1 === void 0 ? void 0 : step1.classList.add("disappear");
         step1 === null || step1 === void 0 ? void 0 : step1.classList.add("disappeared"); // Add this class to make the previous form invisible
         step2 === null || step2 === void 0 ? void 0 : step2.classList.add("appear");
     }
     // On step 2
+    if (stepClicked === 2) {
+        removeAnimationClasses(step2);
+        removeAnimationClasses(step3);
+        step2 === null || step2 === void 0 ? void 0 : step2.classList.add("disappear");
+        step2 === null || step2 === void 0 ? void 0 : step2.classList.add("disappeared");
+        step3 === null || step3 === void 0 ? void 0 : step3.classList.add("appear");
+    }
     console.log("val: ", stepClicked);
 };
 var handlePrev = function (event) {
@@ -26,12 +40,18 @@ var handlePrev = function (event) {
     var stepClicked = parseInt(event.target.dataset.val);
     // On step 2
     if (stepClicked === 2) {
-        step1 === null || step1 === void 0 ? void 0 : step1.classList.remove("disappear");
-        step2 === null || step2 === void 0 ? void 0 : step2.classList.remove("appear");
+        removeAnimationClasses(step1);
+        removeAnimationClasses(step2);
         step2 === null || step2 === void 0 ? void 0 : step2.classList.add("appear-rev");
         step1 === null || step1 === void 0 ? void 0 : step1.classList.add("disappear-rev");
     }
     // On step 3
+    if (stepClicked === 3) {
+        removeAnimationClasses(step2);
+        removeAnimationClasses(step3);
+        step2 === null || step2 === void 0 ? void 0 : step2.classList.add("disappear-rev");
+        step3 === null || step3 === void 0 ? void 0 : step3.classList.add("appear-rev");
+    }
 };
 /* Initialize event listeners */
 var init = (function () {
--- multistep-form/index.ts
+++ multistep-form/index.ts
@@ -11,6 +11,14 @@ const prevStep2 = document.querySelector("#prev-step-2");
 
 const prevStep3 = document.querySelector("#prev-step-3");
 
+const animationClasses = ["disappear", "disappear-rev", "appear", "appear-rev"];
+
+const removeAnimationClasses = function (step: Element | null) {
+  step?.classList.forEach((stepClass: string) => {
+    if (animationClasses.includes(stepClass)) step.classList.remove(stepClass);
+  });
+};
+
 const handleNext = function (event: Event) {
   event.preventDefault();
   const stepClicked = parseInt(
@@ -19,15 +27,21 @@ const handleNext = function (event: Event) {
 
   // On step 1
   if (stepClicked === 1) {
-    step1?.classList.remove("disappear-rev");
-    step2?.classList.remove("appear-rev");
-
+    removeAnimationClasses(step1);
+    removeAnimationClasses(step2);
     step1?.classList.add("disappear");
     step1?.classList.add("disappeared"); // Add this class to make the previous form invisible
     step2?.classList.add("appear");
   }
 
   // On step 2
+  if (stepClicked === 2) {
+    removeAnimationClasses(step2);
+    removeAnimationClasses(step3);
+    step2?.classList.add("disappear");
+    step2?.classList.add("disappeared");
+    step3?.classList.add("appear");
+  }
 
   console.log("val: ", stepClicked);
 };
@@ -40,14 +54,19 @@ const handlePrev = function (event: Event) {
 
   // On step 2
   if (stepClicked === 2) {
-    step1?.classList.remove("disappear");
-    step2?.classList.remove("appear");
-
+    removeAnimationClasses(step1);
+    removeAnimationClasses(step2);
     step2?.classList.add("appear-rev");
     step1?.classList.add("disappear-rev");
   }
 
   // On step 3
+  if (stepClicked === 3) {
+    removeAnimationClasses(step2);
+    removeAnimationClasses(step3);
+    step2?.classList.add("disappear-rev");
+    step3?.classList.add("appear-rev");
+  }
 };
 
 /* Initialize event listeners */

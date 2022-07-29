a154bbb1076c4a29664d55f2d589b9c674d3aa44
unknown <rahulrao0209@gmail.com>
2022-07-29 15:01:13 +0530

    added basic functionality for moving between step1 and 2 with animations

--- multistep-form/index.js
+++ multistep-form/index.js
@@ -9,12 +9,34 @@ var prevStep2 = document.querySelector("#prev-step-2");
 var prevStep3 = document.querySelector("#prev-step-3");
 var handleNext = function (event) {
     event.preventDefault();
-    console.log("TEST");
-    if (step1) {
-        step1.classList.toggle("disappear");
+    var stepClicked = parseInt(event.target.dataset.val);
+    // On step 1
+    if (stepClicked === 1) {
+        step1 === null || step1 === void 0 ? void 0 : step1.classList.remove("disappear-rev");
+        step2 === null || step2 === void 0 ? void 0 : step2.classList.remove("appear-rev");
+        step1 === null || step1 === void 0 ? void 0 : step1.classList.add("disappear");
+        step1 === null || step1 === void 0 ? void 0 : step1.classList.add("disappeared"); // Add this class to make the previous form invisible
+        step2 === null || step2 === void 0 ? void 0 : step2.classList.add("appear");
     }
+    // On step 2
+    console.log("val: ", stepClicked);
+};
+var handlePrev = function (event) {
+    event.preventDefault();
+    var stepClicked = parseInt(event.target.dataset.val);
+    // On step 2
+    if (stepClicked === 2) {
+        step1 === null || step1 === void 0 ? void 0 : step1.classList.remove("disappear");
+        step2 === null || step2 === void 0 ? void 0 : step2.classList.remove("appear");
+        step2 === null || step2 === void 0 ? void 0 : step2.classList.add("appear-rev");
+        step1 === null || step1 === void 0 ? void 0 : step1.classList.add("disappear-rev");
+    }
+    // On step 3
 };
 /* Initialize event listeners */
 var init = (function () {
     nextStep1 === null || nextStep1 === void 0 ? void 0 : nextStep1.addEventListener("click", handleNext);
+    nextStep2 === null || nextStep2 === void 0 ? void 0 : nextStep2.addEventListener("click", handleNext);
+    prevStep2 === null || prevStep2 === void 0 ? void 0 : prevStep2.addEventListener("click", handlePrev);
+    prevStep3 === null || prevStep3 === void 0 ? void 0 : prevStep3.addEventListener("click", handlePrev);
 })();
--- multistep-form/index.ts
+++ multistep-form/index.ts
@@ -13,14 +13,49 @@ const prevStep3 = document.querySelector("#prev-step-3");
 
 const handleNext = function (event: Event) {
   event.preventDefault();
-  console.log("TEST");
+  const stepClicked = parseInt(
+    (event.target as HTMLElement).dataset.val as string
+  );
 
-  if (step1) {
-    step1.classList.toggle("disappear");
+  // On step 1
+  if (stepClicked === 1) {
+    step1?.classList.remove("disappear-rev");
+    step2?.classList.remove("appear-rev");
+
+    step1?.classList.add("disappear");
+    step1?.classList.add("disappeared"); // Add this class to make the previous form invisible
+    step2?.classList.add("appear");
   }
+
+  // On step 2
+
+  console.log("val: ", stepClicked);
+};
+
+const handlePrev = function (event: Event) {
+  event.preventDefault();
+  const stepClicked = parseInt(
+    (event.target as HTMLElement).dataset.val as string
+  );
+
+  // On step 2
+  if (stepClicked === 2) {
+    step1?.classList.remove("disappear");
+    step2?.classList.remove("appear");
+
+    step2?.classList.add("appear-rev");
+    step1?.classList.add("disappear-rev");
+  }
+
+  // On step 3
 };
 
 /* Initialize event listeners */
 const init = (function () {
   nextStep1?.addEventListener("click", handleNext);
+
+  nextStep2?.addEventListener("click", handleNext);
+  prevStep2?.addEventListener("click", handlePrev);
+
+  prevStep3?.addEventListener("click", handlePrev);
 })();

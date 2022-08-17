747720cdf752486d4f220e97e2631e204b8779b0
unknown <rahulrao0209@gmail.com>
2022-08-17 20:35:20 +0530

    adding initial styling

--- rotating-flight-widget/index.ts
+++ rotating-flight-widget/index.ts
@@ -8,10 +8,40 @@ type FlightDetails = {
 
 const flights: FlightDetails[] = [
   {
-    time: "",
-    destination: "",
-    flight: "",
-    gate: "",
-    remarks: "",
+    time: "08:11",
+    destination: "OMAN",
+    flight: "OX 203",
+    gate: "A 01",
+    remarks: "ON TIME",
+  },
+  {
+    time: "12:39",
+    destination: "LONDON",
+    flight: "CL 320",
+    gate: "C 31",
+    remarks: "DELAYED",
+  },
+  {
+    time: "15:45",
+    destination: "GOA",
+    flight: "AI 720",
+    gate: "A 26",
+    remarks: "ON TIME",
+  },
+  {
+    time: "18:30",
+    destination: "DUBAI",
+    flight: "QA 737",
+    gate: "B 24",
+    remarks: "CANCELLED",
+  },
+  {
+    time: "8:30",
+    destination: "FRANKFURT",
+    flight: "LU 411",
+    gate: "D 22",
+    remarks: "ON TIME",
   },
 ];
+
+const tableBody = document.querySelector("#table-body");

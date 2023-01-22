d1ecc2bc82e880398271d1a88591ef12c72e5d64
Rahul Rao <63695122+rahulrao0209@users.noreply.github.com>
2023-01-22 13:37:56 +0530

    Adding dummy tasks

--- drag-and-drop/src/index.ts
+++ drag-and-drop/src/index.ts
@@ -1,1 +1,35 @@
-export const init = () => {};
+/* Get the todo and done lists (drop areas) */
+const getDomLists = function () {
+  const todoList = document.querySelector(
+    ".list__tasks--todo"
+  )! as HTMLUListElement;
+  const doneList = document.querySelector(
+    ".list__tasks--done"
+  )! as HTMLUListElement;
+
+  return {
+    todoList,
+    doneList,
+  };
+};
+
+const handleDragEnter = function (event: Event) {
+  event.preventDefault();
+  console.log("I am here: ", event.target);
+};
+
+const handleOnDrop = function (event: Event) {
+  event.preventDefault();
+};
+
+/* Init function */
+const init = (function () {
+  const { todoList, doneList } = getDomLists();
+
+  /* Add the drop area event handlers to the lists */
+  todoList.addEventListener("dragenter", handleDragEnter);
+  doneList.addEventListener("dragenter", handleDragEnter);
+
+  todoList.addEventListener("ondrop", handleOnDrop);
+  doneList.addEventListener("ondrop", handleOnDrop);
+})();

4dc983ff04b8d49205bcad276449d7b840eb1bd7
Rahul Rao <rahulrao0209@gmail.com>
2024-08-29 11:17:57 +0530

    Date-range-picker | Add PickerController and create DayController.

--- date-range-picker/src/components/picker/index.tsx
+++ date-range-picker/src/components/picker/index.tsx
@@ -3,20 +3,6 @@ import { getMonthData } from "../../utils";
 import "./index.css";
 
 const WEEK_DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-// const MONTH_NAMES = [
-//   "Jan",
-//   "Feb",
-//   "Mar",
-//   "Apr",
-//   "May",
-//   "Jun",
-//   "Jul",
-//   "Aug",
-//   "Sep",
-//   "Oct",
-//   "Nov",
-//   "Dec",
-// ];
 
 const Picker = () => {
   const { previousMonthDays, currentMonthDays, nextMonthDays } = getMonthData();
--- date-range-picker/src/components/pickerController/index.tsx
+++ date-range-picker/src/components/pickerController/index.tsx
@@ -1,7 +1,60 @@
+import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
 import "./index.css";
 
+const MONTH_NAMES = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
 const PickerController = () => {
-  return <div className="picker-controller">Picker Controller</div>;
+  const YearController = () => {};
+  const MonthController = () => {};
+
+  interface DayControllerProps {
+    month: number;
+    year: number;
+  }
+
+  const DayController = ({ month, year }: DayControllerProps) => {
+    const monthName = MONTH_NAMES[month];
+
+    return (
+      <div className="day-controller">
+        <button className="day-controller__back">
+          <span className="day-controller__back-icon">
+            <MdArrowBackIos />
+          </span>
+        </button>
+        <button className="day-controller__btn">{`${monthName}, ${year}`}</button>
+        <button className="day-controller__next">
+          <span className="day-controller__next-icon">
+            <MdArrowForwardIos />
+          </span>
+        </button>
+      </div>
+    );
+  };
+
+  return (
+    <div className="picker-controller">
+      <div>
+        <DayController
+          month={new Date().getMonth()}
+          year={new Date().getFullYear()}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default PickerController;
